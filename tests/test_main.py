cc2398cd316fbaa9cc302a3047f493816f73f7d8
hatone <b1006006@gmail.com>
2013-02-07 18:47:21 +0900

    i'm sorry

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,3 +6,14 @@ class TestFunctions(unittest.TestCase):
   def test_adding(self):
     self.failUnlessEqual( adding(1,2), 3)
 
+  def test_split_general_frames(self):
+    second_sample = "9-9-9-9-9-9-9-9-9-9-"
+    third_sample = "5/5/5/5/5/5/5/5/5/5/5"
+
+    frames = []
+    for var in range (0,9):
+      frames[0]=second_sample[var]+second_sample[var+1]
+      var = var + 1
+
+
+
